3431f784922a932682f9780561137dacf9c208d5
Frank McCown <fmccown@harding.edu>
2018-01-29 14:09:15 -0600

    Add project files

---
+++ Boids/Assets/Boid.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+
+    [Header("Set Dynamically")]
+    public Rigidbody rigid;
+
+    private Neighborhood neighborhood;
+
+    public Vector3 pos
+    {
+        get { return transform.position; }
+        set { transform.position = value; }
+    }
+
+    private void Awake()
+    {
+        neighborhood = GetComponent<Neighborhood>();
+        rigid = GetComponent<Rigidbody>();
+
+        pos = Random.insideUnitSphere * Spawner.S.spawnRadius;
+
+        Vector3 vel = Random.onUnitSphere * Spawner.S.velocity;
+        rigid.velocity = vel;
+
+        LookAhead();
+
+        Color randColor = Color.black;
+        while (randColor.r + randColor.g + randColor.b < 1.0f)
+        {
+            randColor = new Color(Random.value, Random.value, Random.value);
+        }
+
+        Renderer[] rends = gameObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in rends)
+        {
+            r.material.color = randColor;
+        }
+
+        TrailRenderer tRend = GetComponent<TrailRenderer>();
+        tRend.material.SetColor("_TintColor", randColor);
+
+    }
+
+    void LookAhead()
+    {
+        // Look in the direction you are flying
+        transform.LookAt(pos + rigid.velocity);
+    }
+
+    // Called once per physics update (50 times per sec)
+    void FixedUpdate()
+    {
+        Vector3 vel = rigid.velocity;
+        Spawner spn = Spawner.S;
+
+        // Collision avoidance
+        Vector3 velAvoid = Vector3.zero;
+        Vector3 tooClosePos = neighborhood.avgClosePos;
+
+        // If zero then no need to react
+        if (tooClosePos != Vector3.zero)
+        {
+            velAvoid = pos - tooClosePos;
+            velAvoid.Normalize();
+            velAvoid *= spn.velocity;
+        }
+
+        // Try matching velocity of neighbors
+        Vector3 velAlign = neighborhood.avgVel;
+        if (velAlign != Vector3.zero)
+        {
+            // Only interested in direction, so normalize velocity
+            velAlign.Normalize();
+
+            // Set to chosen speed
+            velAlign *= spn.velocity;
+        }
+
+        // Move to center of local neighbors
+        Vector3 velCenter = neighborhood.avgPos;
+        if (velCenter != Vector3.zero)
+        {
+            velCenter -= transform.position;
+            velCenter.Normalize();
+            velCenter *= spn.velocity;
+        }
+
+        // Move toward Attractor
+        Vector3 delta = Attractor.POS - pos;
+
+        // Attracted to or from?
+        bool attracted = (delta.magnitude > spn.attractPushDist);
+        Vector3 velAttract = delta.normalized * spn.velocity;
+
+        // Apply all velocities
+        float fdt = Time.fixedDeltaTime;
+        if (velAvoid != Vector3.zero)
+        {
+            vel = Vector3.Lerp(vel, velAvoid, spn.collAvoid * fdt);
+        }
+        else
+        {
+            if (velAlign != Vector3.zero)
+            {
+                vel = Vector3.Lerp(vel, velAlign, spn.velMatching * fdt);
+            }
+            if (velCenter != Vector3.zero)
+            {
+                vel = Vector3.Lerp(vel, velAlign, spn.flockCentering * fdt);
+            }
+            if (velAttract != Vector3.zero)
+            {
+                if (attracted)
+                {
+                    vel = Vector3.Lerp(vel, velAttract, spn.attractPull * fdt);
+                }
+                else
+                {
+                    vel = Vector3.Lerp(vel, -velAttract, spn.attractPush * fdt);
+                }
+            }
+        }
+                
+
+        // Set vel to the velocity set on the Spawner singleton
+        vel = vel.normalized * spn.velocity;
+
+        // assign to this Rigidbody
+        rigid.velocity = vel;
+
+        LookAhead();
+    }
+
+}
---
+++ Boids/Assets/Spawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    static public Spawner S;
+    static public List<Boid> boids;
+
+    [Header("Set in Insepctor: Spawning")]
+    public GameObject boidPrefab;
+    public Transform boidAnchor;
+    public int numBoids = 100;
+    public float spawnRadius = 100f;
+    public float spawnDelay = 0.1f;
+
+    [Header("Set in Insepctor: Boids")]
+    public float velocity = 30;
+    public float neighborDist = 30;
+    public float collDist = 4;
+    public float velMatching = 0.25f;
+    public float flockCentering = 0.2f;
+    public float collAvoid = 2;
+    public float attractPull = 2;
+    public float attractPush = 2;
+    public float attractPushDist = 5;
+
+    void Awake()
+    {
+        // Set singleton
+        S = this;
+        boids = new List<Boid>();
+        InstantiateBoid();
+    }
+
+    public void InstantiateBoid()
+    {
+        GameObject go = Instantiate(boidPrefab);
+        Boid b = go.GetComponent<Boid>();
+        b.transform.SetParent(boidAnchor);
+        boids.Add(b);
+        if (boids.Count < numBoids)
+        {
+            Invoke("InstantiateBoid", spawnDelay);
+        }
+    }
+}
